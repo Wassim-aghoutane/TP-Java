221adda32cccb5afc7a17211873e7cd5566569b2
Wassim-aghoutane <ouasymaghoutane14@gmail.com>
2024-12-22 20:02:57 +0100

    add test test Sort Person

--- src/test/java/com/example/PersonServiceTest.java
+++ src/test/java/com/example/PersonServiceTest.java
@@ -61,4 +61,26 @@ public class PersonServiceTest {
 
         assertThat(peopleWithoutBob).containsExactlyInAnyOrderElementsOf(people);
     }
+
+    @Test
+    public void testSortPerson() {
+        List<Person> people = new ArrayList<>();
+        people.add(Person.builder().firstName("Hamid").familyName("Jamila").build());
+        people.add(Person.builder().firstName("Martin").familyName("Bob").build());
+        people.add(Person.builder().firstName("Hamid").familyName("Charles").build());
+        people.add(Person.builder().firstName("Bernard").familyName("Charles").build());
+
+        // Tri de la liste de personnes
+        Collections.sort(people);
+
+        assertThat(people.get(0))
+                .isEqualTo(Person.builder().firstName("Martin").familyName("Bob").build());
+        assertThat(people.get(1))
+                .isEqualTo(Person.builder().firstName("Bernard").familyName("Charles").build());
+        assertThat(people.get(2))
+                .isEqualTo(Person.builder().firstName("Hamid").familyName("Charles").build());
+        assertThat(people.get(3))
+                .isEqualTo(Person.builder().firstName("Hamid").familyName("Jamila").build());
+
+    }
 }
